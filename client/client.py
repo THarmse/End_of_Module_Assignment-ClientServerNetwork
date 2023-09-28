9ba9e954948cd863135c369da95b1e8b86e06fc4
m38109 <m38109@gmail.com>
2023-09-28 09:05:58 +0800

    complete the code

--- client/client.py
+++ client/client.py
@@ -1,11 +1,14 @@
 # Set up a simple client/server network
 # Set up Client
 
+""""The `socket` module provides a low-level interface for network communication. """
 import socket
 
 CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = '127.0.0.1'
 PORT = 1245
 
-CLIENT_SOCKET.connect((HOST,PORT)) 
+CLIENT_SOCKET.connect((HOST, PORT))
+CLIENT_SOCKET.send(b'Hello')
 
+CLIENT_SOCKET.close()
--- server/server.py
+++ server/server.py
@@ -1,14 +1,23 @@
 # Set up a simple client/server network
-# Server setup 
+# Server setup
 
+""""The `socket` module provides a low-level interface for network communication."""
 import socket
 
-#Server setup
+# Server setup
 SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = '127.0.0.1' #Server IP address (localhost in this case)
-PORT = 1245 #Port to listen on
+HOST = '127.0.0.1'  # Server IP address (localhost in this case)
+PORT = 1245  # Port to listen on
 
-SERVER_SOCKET.bind((HOST,PORT))
-SERVER_SOCKET.listen(1) #Listen to one client at once
+SERVER_SOCKET.bind((HOST, PORT))
+SERVER_SOCKET.listen(1)  # Listen to one client at once
 
 print(f"Server is listening on {HOST}:{PORT}")
+
+while True:
+    conn, addr = SERVER_SOCKET.accept()
+    print(f"Connected by {addr}")
+    DATA = conn.recv(1024)
+    print("Recieved:",DATA.decode())
+
+    conn.close()
