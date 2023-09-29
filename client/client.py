4195439f19193f24f17169825aabd8c8f83c1e61
m38109 <m38109@gmail.com>
2023-09-29 21:58:27 +0800

    add serialization to server/client

--- client/client.py
+++ client/client.py
@@ -3,12 +3,68 @@
 
 """"The `socket` module provides a low-level interface for network communication. """
 import socket
+"""serializing and deserializing ojects"""
+import pickle
+import json
+import xml.etree.ElementTree as ET
+
+# Define a class that allow to reuse the same data and
+# switch between serialization formats more easily
+
+class DataSerializer:
+    def __init__(self, data):
+        self.data = data
+
+    def serialize_binary(self):
+        return pickle.dumps(self.data)
+
+    def serialize_json(self):
+        return json.dumps(self.data).encode()
+
+    def serialize_xml(self):
+        root = ET.Element("data")
+        for key, value in self.data.items():
+            child = ET.Element(key)
+            child.text = str(value)
+            root.append(child)
+        return ET.tostring(root, encoding='utf-8')
+    
+# Creat a simple dictionary
+MY_DICT = {
+    'fruit': 'apple',
+    'colour': 'red'
+}
+# serilze data
+serializer = DataSerializer(MY_DICT)
 
 CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = 'localhost'
 PORT = 1245
 
 CLIENT_SOCKET.connect((HOST, PORT))
-CLIENT_SOCKET.send(b'Hello')
+
+while True:
+    FORMAT = input(
+        "Choose a serialization format: B=Binary, J=Json, X=XML:").lower()
+
+    if FORMAT == "b":
+        serialized_binary = serializer.serialize_binary()
+        CLIENT_SOCKET.send(FORMAT.encode())
+        CLIENT_SOCKET.send(serialized_binary)
+        break
+    elif FORMAT == "j":
+        serialized_json = serializer.serialize_json()
+        CLIENT_SOCKET.send(FORMAT.encode())
+        CLIENT_SOCKET.send(serialized_json)
+        break
+    elif FORMAT == "x":
+        serialized_xml = serializer.serialize_xml()
+        CLIENT_SOCKET.send(FORMAT.encode())
+        CLIENT_SOCKET.send(serialized_xml)
+        break
+    else:
+        print("Error. Please try again.")
+    break
+
 
 CLIENT_SOCKET.close()
--- server/server.py
+++ server/server.py
@@ -3,6 +3,55 @@
 
 """"The `socket` module provides a low-level interface for network communication."""
 import socket
+"""serializing and deserializing ojects"""
+import pickle
+import json
+import xml.etree.ElementTree as ET
+
+# Define a class that allow to reuse the same data and
+# switch between different formats more easily
+
+class DataDeserializer:
+    def __init__(self, data):
+        self.data = data
+
+    def deserialize_binary(self):
+        return pickle.loads(self.data)
+
+    def deserialize_json(self):
+        return json.loads(self.data)
+
+    def deserialize_xml(self):
+        root = ET.fromstring(self.data)
+        data_dict = {}
+        for child in root:
+            data_dict[child.tag] = child.text
+        return data_dict
+    
+def Print_Screen(user_input_for_print):
+    if user_input_for_print == "y":
+        return True
+    elif user_input == "n":
+        return False
+    else:
+        print("Error. Please try again.")
+        return Print_Screen()
+
+def Save_to_log(user_input_for_save):
+    if user_input_for_save == "y":
+        return True
+    elif user_input == "n":
+        return False
+    else:
+        print("Error. Please try again.")
+        return Print_Screen()
+
+def Write_to_log(DATA):
+    with open ("server/server_log.txt",'a') as file:
+        file.write(str(DATA)+ '\n')
+
+
+
 
 # Server setup
 SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,9 +66,32 @@ print(f"Server is listening on {HOST}:{PORT}")
 while True:
     conn, addr = SERVER_SOCKET.accept()
     print(f"Connected by {addr}")
-    DATA = conn.recv(1024)
-    print("Recieved:",DATA.decode())
+    
+    FORMAT = conn.recv(1024).decode()
+    RECIEVED_DATA = conn.recv(1024)
+    deserilaizer = DataDeserializer (RECIEVED_DATA)
 
+    while True:
+        user_input1 = input("Print the received data on screen? (Y/N)").lower()
+        user_input2 = input("Save the received data to log? (Y/N)").lower()
+        
+        if FORMAT == "b":
+            deserilazed_data = deserilaizer.deserialize_binary()
+        elif FORMAT == "j":
+            deserilazed_data = deserilaizer.deserialize_json()
+        elif FORMAT == "x":
+            deserilazed_data = deserilaizer.deserialize_xml()
+        else:
+            print("Error. Invalid format.")
+            conn.close()
+            break
+
+        if Print_Screen(user_input1):
+            print(deserilazed_data)
+        if Save_to_log(user_input2):
+            Write_to_log(deserilazed_data)
+
+        break  #exit the loop after processing one set of data
     conn.close()
     break
 
