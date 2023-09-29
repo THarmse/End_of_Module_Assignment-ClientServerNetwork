83eaba2a11d10cdf0c6e9814984662122d17ca87
m38109 <m38109@gmail.com>
2023-09-30 00:57:31 +0800

    complete Encrypting file transmission

--- client/client.py
+++ client/client.py
@@ -7,10 +7,10 @@ import socket
 import pickle
 import json
 import xml.etree.ElementTree as ET
+from cryptography.fernet import Fernet
 
 # Define a class that allow to reuse the same data and
 # switch between serialization formats more easily
-
 class DataSerializer:
     def __init__(self, data):
         self.data = data
@@ -37,6 +37,11 @@ MY_DICT = {
 # serilze data
 serializer = DataSerializer(MY_DICT)
 
+#Crate a file
+Text_to_file = input ("Pleae write someting to the file:")
+with open ('client/new_file.txt','w') as file:
+    file.write(Text_to_file)
+
 CLIENT_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = 'localhost'
 PORT = 1245
@@ -44,6 +49,7 @@ PORT = 1245
 CLIENT_SOCKET.connect((HOST, PORT))
 
 while True:
+    #Serialize the data and send to the server
     FORMAT = input(
         "Choose a serialization format: B=Binary, J=Json, X=XML:").lower()
 
@@ -51,20 +57,54 @@ while True:
         serialized_binary = serializer.serialize_binary()
         CLIENT_SOCKET.send(FORMAT.encode())
         CLIENT_SOCKET.send(serialized_binary)
-        break
     elif FORMAT == "j":
         serialized_json = serializer.serialize_json()
         CLIENT_SOCKET.send(FORMAT.encode())
         CLIENT_SOCKET.send(serialized_json)
-        break
     elif FORMAT == "x":
         serialized_xml = serializer.serialize_xml()
         CLIENT_SOCKET.send(FORMAT.encode())
         CLIENT_SOCKET.send(serialized_xml)
-        break
     else:
         print("Error. Please try again.")
-    break
 
+    #encrypted the text in file and sent it to the server
+    
+    while True:
+        user_encrypt_or_not = input("Do you want to encrypt the text in file?(Y/N)")
+        if user_encrypt_or_not == "y":
+            encrypt_text = True
+            CLIENT_SOCKET.send(b"The text is encrypted.")
+            break
+        elif user_encrypt_or_not == "n":
+            encrypt_text = False
+            CLIENT_SOCKET.send(b'The text is not encrypted.')
+            break
+        else:
+            print("Error. Please try again.")
+
+    if encrypt_text:
+        # Key for encryption (keep this secret)
+        key = Fernet.generate_key()
+        print(key)
+        fernet = Fernet(key)
+        CLIENT_SOCKET.send(key)
+
+         # Encrypt the text in the file
+        with open('client/new_file.txt', 'rb') as encrypted_doc:
+            encrypted_text = fernet.encrypt(encrypted_doc.read())
+        #write a the encrypted text in to a file
+        with open('client/encrypted_file.txt', 'wb') as file_to_send:
+            file_to_send.write(encrypted_text)
+        
+        # Send the file name to server
+        CLIENT_SOCKET.send(b"encrypted_file.txt")
+        CLIENT_SOCKET.send(encrypted_text)
+        
+    else:
+        CLIENT_SOCKET.send(b"new_file.txt")
+        pickled_data = pickle.dumps(Text_to_file)
+        CLIENT_SOCKET.send(pickled_data)
+    break
 
 CLIENT_SOCKET.close()
--- server/server.py
+++ server/server.py
@@ -7,6 +7,7 @@ import socket
 import pickle
 import json
 import xml.etree.ElementTree as ET
+from cryptography.fernet import Fernet
 
 # Define a class that allow to reuse the same data and
 # switch between different formats more easily
@@ -34,8 +35,6 @@ def Write_to_log(DATA):
         file.write(str(DATA)+ '\n')
 
 
-
-
 # Server setup
 SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = 'localhost'  # Server IP address (localhost in this case)
@@ -46,6 +45,7 @@ SERVER_SOCKET.listen(1)  # Listen to one client at once
 
 print(f"Server is listening on {HOST}:{PORT}")
 
+
 while True:
     conn, addr = SERVER_SOCKET.accept()
     print(f"Connected by {addr}")
@@ -53,39 +53,85 @@ while True:
     FORMAT = conn.recv(1024).decode()
     RECIEVED_DATA = conn.recv(1024)
     deserilaizer = DataDeserializer (RECIEVED_DATA)
-
     while True:
-        while True:
-            user_input1 = input("Print the received data on screen? (Y/N)").lower()
-            user_input2 = input("Save the received data to log? (Y/N)").lower()
-            if user_input1 == "y" and user_input2 == "y":
-                Print_Screen = True
-                Save_to_log = True
-                break
-            elif user_input1 =="n" and user_input2 == "n":
-                Print_Screen = False
-                Save_to_log = False
-                break
-            else:
-                print("Error. Please try again")
-
-        if FORMAT == "b":
-            deserilazed_data = deserilaizer.deserialize_binary()
-        elif FORMAT == "j":
-            deserilazed_data = deserilaizer.deserialize_json()
-        elif FORMAT == "x":
-            deserilazed_data = deserilaizer.deserialize_xml()
-        else:
-            print("Error. Invalid format.")
-            conn.close()
+        user_input1 = input("Print the received data on screen? (Y/N)").lower()
+        user_input2 = input("Save the received data to log? (Y/N)").lower()
+        if user_input1 == "y" and user_input2 == "y":
+            Print_Screen = True
+            Save_to_log = True
             break
+        elif user_input1 =="n" and user_input2 == "n":
+            Print_Screen = False
+            Save_to_log = False
+            break
+        else:
+            print("Error. Please try again")
+    try:
+        while True:
+            if not FORMAT or not RECIEVED_DATA:
+                break
+            if FORMAT == "b":
+                deserilazed_data = deserilaizer.deserialize_binary()
+            elif FORMAT == "j":
+                deserilazed_data = deserilaizer.deserialize_json()
+            elif FORMAT == "x":
+                deserilazed_data = deserilaizer.deserialize_xml()
 
-        if Print_Screen:
-            print(deserilazed_data)
-        if Save_to_log:
-            Write_to_log(deserilazed_data)
+            if Print_Screen:
+                print(deserilazed_data)
+            if Save_to_log:
+                Write_to_log(deserilazed_data)
 
-        break  #exit the loop after processing one set of data
+            break  #exit the loop after processing one set of data
+
+        ENCRYPTED_OT_NOT = conn.recv(1024).decode()
+        if not ENCRYPTED_OT_NOT:
+            break
+        elif ENCRYPTED_OT_NOT == "The text is encrypted." :
+            print (ENCRYPTED_OT_NOT)
+            key_message = conn.recv(2048)    
+            if key_message:
+                print ("Here is the key: ", key_message)
+            else:
+                break
+
+            fernet = Fernet(key_message)
+            # Receive the encrypted filename
+            filename = conn.recv(1024).decode()  
+            encrypted_text = conn.recv(1024).decode()
+            if not filename or not encrypted_text:
+                break
+            decrypted_text = fernet.decrypt(encrypted_text).decode()
+            with open (filename,'w') as recieved_file:
+                recieved_file.write(decrypted_text)
+            if Print_Screen:
+                print(decrypted_text)
+            if Save_to_log:
+                Write_to_log(decrypted_text)
+            break
+        elif ENCRYPTED_OT_NOT == "The text is not encrypted.":
+            print (ENCRYPTED_OT_NOT)
+            filename = conn.recv(1024).decode()
+            print(filename)
+            file_text = conn.recv(1024)
+            unpickled_text = pickle.loads(file_text)
+            print(unpickled_text)
+            if not filename or not file_text:
+                break
+            with open(filename, 'w') as create_file:
+                create_file.write(unpickled_text)
+            if Print_Screen:
+                print(unpickled_text)
+            if Save_to_log:
+                Write_to_log(unpickled_text)
+            break                
+                
+    except Exception as e:
+        print(f"An exception occurred: {str(e)}")
+        # Optionally, log the exception to a log file
+        with open("server\error_log.txt", "a") as error_file:
+            error_file.write(f"Exception: {str(e)}\n")
+
     conn.close()
     break
 
