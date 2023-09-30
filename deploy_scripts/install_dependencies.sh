a1ca099e82fe5f9298bb7f83958ac77a521033df
TheodorHarmse <t.harmse@liverpool.ac.uk>
2023-09-30 14:08:25 +0400

    Theodor Harmse: updating appespec.yml to use /opt/liverpool directory, added comments.  Added permission for requirements.txt in appspec

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-cd "$PWD" || exit 1  # exit if the directory does not exist
+cd /opt/liverpool || exit 1  # exit if the directory does not exist
 
 # Check if pip is installed; if not, install it
-if ! command -v pip &> /dev/null
+if ! command -v pip3 &> /dev/null
 then
+    # Download the pip installation script
     sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    # Install pip using the downloaded script
     sudo python3 get-pip.py
+    # Remove the downloaded script
     sudo rm get-pip.py
 fi
 
-# Install Python dependencies
-sudo pip install -r requirements.txt
+# Install Python dependencies from requirements.txt.
+sudo pip3 install -r requirements.txt
