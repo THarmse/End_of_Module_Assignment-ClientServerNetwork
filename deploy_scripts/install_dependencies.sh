085ca0d6b475bb4aec1c70009226a4950fde60a4
TheodorHarmse <t.harmse@liverpool.ac.uk>
2023-09-30 14:01:53 +0400

    Theodor Harmse: updating appespec.yml to allow for permissions. Updated dependencies to check if pip is installed, if not, to install.

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-cd /opt/liverpool
-pip install -r requirements.txt
+
+cd "$PWD" || exit 1  # exit if the directory does not exist
+
+# Check if pip is installed; if not, install it
+if ! command -v pip &> /dev/null
+then
+    sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo python3 get-pip.py
+    sudo rm get-pip.py
+fi
+
+# Install Python dependencies
+sudo pip install -r requirements.txt
