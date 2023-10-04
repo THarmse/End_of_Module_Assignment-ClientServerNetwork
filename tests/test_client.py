8f23da434f55d727de24eeb1bf381571271a7d9a
Yuhan Wu <143881845+m38109@users.noreply.github.com>
2023-10-04 23:52:44 +0800

    Update test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,77 +1,109 @@
 import unittest
 import json
+from unittest.mock import patch, Mock
 import base64
-from unittest.mock import patch
-from client_app import app
-import tempfile
+from client.client_app import app
+import pickle
+import xml.etree.ElementTree as ET
 
 class ClientTestCase(unittest.TestCase):
-    
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    @patch('client_app.encryption.encrypt_data')
-    # Mock the encryption function
-    @patch('client_app.create_temp_text_file')
-    # Mock the text file creation function
-    @patch('socket.socket')
+    def test_Json_without_encrypt_not_file(self):
     
-    def test_send_data(self, mock_socket, mock_create_temp_text_file, mock_encrypt_data):
-        
         sample_data = {
-            "data": {"key1": "value1"},
-            "format": "JSON",
-            "encrypt": True,
-            "asTextFile": True
-        }  # Prepare a sample JSON request
-
-        mock_encrypt_data.return_value = json.dumps(sample_data['data'])
-        # Mock the behavior of the encryption function
-
-        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-            temp_file.write(b'file content')  # Write binary content to the temporary file
-            temp_file.flush()  # Flush to ensure content is written
-            temp_file_path = temp_file.name
-
-        mock_create_temp_text_file.return_value = temp_file_path
-        # Mock the behavior of the text file creation function
+            "data": {"key": "value"},
+            "format": "JSON",    # Test JSON serialization
+            "encrypt": False,
+            "asTextFile": False
+        }
 
         response = self.app.post('/send_data', json=sample_data)
-        # Send a POST request to the /send_data route
-
         self.assertEqual(response.status_code, 200)
-        # Check if the response status code is 200 (OK)
 
-        response_data = json.loads(response.data)
-        self.assertEqual(response_data['status'], 'success')
-        # Check if the response contains a "status" field with a
-        # value of "success"
+        # Check if the response contains the expected serialized data
+        expected_data = json.dumps(sample_data['data'])
+        self.assertEqual(response.json['data'], expected_data)
 
-        mock_encrypt_data.assert_called_once_with(json.dumps(sample_data['data']).encode())
-        # Assert that the encryption function was called with the correct arguments
-
-        mock_create_temp_text_file.assert_called_once()
-        # Assert that the text file creation function was called since asTextFile is True
-
-        # Encode the file content as bytes and then base64 encode it
-        with open(temp_file_path, "rb") as file:
-            file_content_bytes = file.read()
-            serialized_data = base64.b64encode(file_content_bytes).decode()
-
-        expected_server_data = {
-            'data': serialized_data,  # Use the base64 encoded content
-            'isEncrypted': True,
-            'isFile': True,
-            'dataFormat': "JSON"
+    def test_Binary_without_encrypt_not_file(self):
+        # Prepare a sample JSON request with different serialize formats
+        sample_data = {
+            "data": {"key": "value"},
+            "format": "Binary",    # Test JSON serialization
+            "encrypt": False,
+            "asTextFile": False
         }
-        # Mock the expected data that should be sent to the server
+        
+        response = self.app.post('/send_data', json=sample_data)
+        self.assertEqual(response.status_code, 200)
 
-        mock_socket_send = mock_socket.return_value.send
-        with patch('socket.socket') as mock_socket:
-            self.app.post('/send_data', json=sample_data)
-            mock_socket_send.assert_called_once_with(json.dumps(expected_server_data).encode())
-            # Check if the data sent to the server matches expectations
+        # Deserialize the response data (base64 decode and unpickle)
+        expected_data_bytes = base64.b64decode(response.json['data'].encode())
+        expected_data = pickle.loads(expected_data_bytes)
+        self.assertEqual(expected_data, sample_data['data'])
+
+    def test_xml_no_encryption_no_file(self):
+        # Prepare a sample request with XML data for XML serialization
+        sample_data = {
+            "data": {"key": "value"},
+            "format": "XML",
+            "encrypt": False,
+            "asTextFile": False
+        }
+
+        response = self.app.post('/send_data', json=sample_data)
+        self.assertEqual(response.status_code, 200)
+
+        # Parse the response data as XML
+        root = ET.fromstring(response.json['data'])
+
+        # Check if the XML contains the correct data
+        self.assertEqual(root.tag, 'root')
+        items = list(root.iter('item'))
+        self.assertEqual(len(items), 1)  # Ensure there is only one <item> element
+
+        key_element = items[0].find('key')
+        value_element = items[0].find('value')
+        self.assertIsNotNone(key_element)
+        self.assertIsNotNone(value_element)
+
+        key_value = key_element.text
+        value_text = value_element.text
+
+        # Verify that the text content of XML elements matches the expected data
+        self.assertEqual(key_value, "key")
+        self.assertEqual(value_text, "value")
+    
+    @patch('client.client_app.encryption.encrypt_data')
+    def test_json_encrypted_no_file(self,mock_encrypt_data):
+        sample_data = {
+            "data": {"key": "value"},
+            "format": "Json",
+            "encrypt": True,
+            "asTextFile": False
+        }
+
+        # Mock the behavior of the encrypt_data function
+        encrypted_data = b"encrypted_data"
+        mock_encrypt_data.return_value = encrypted_data
+
+        json_data = json.dumps(sample_data['data'])
+
+        # Make an HTTP POST request to /send_data with the sample data
+        response = self.app.post('/send_data', json=sample_data)
+
+        # Assert the expected behavior or response
+        self.assertEqual(response.status_code, 200)  # Expected status code
+
+        # Ensure that the encrypt_data function is called with the correct arguments
+        mock_encrypt_data.assert_called_once_with(json_data.encode())
+
+        # Ensure that the response data matches the expected encrypted data
+        expected_response_data = encrypted_data
+        self.assertEqual(response.json['data'], encrypted_data)
+   
 
 if __name__ == '__main__':
     unittest.main()
